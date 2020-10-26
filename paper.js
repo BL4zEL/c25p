7ca560a4767001adf87861a79b79ac3fdacdb849
BL4zEL <70089424+BL4zEL@users.noreply.github.com>
2020-10-26 19:18:16 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,17 @@
+class paper{
+  constructor(x, y, radius) {
+      var options = {
+        'restitution':0.8,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.image=loadImage("sprites/paper.png")
+      this.radius
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      ellipseMode(CENTER)
+      image(this.image,pos.x,pos.y,50,50)
+    }
+}
